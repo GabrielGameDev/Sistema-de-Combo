b8bedd29877aee61bae586cdf1e92df842ee40f3
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2019-03-27 15:19:49 -0300

    Add files via upload

---
+++ Combo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Combo {
+
+	public Hit[] hits;
+}
+
+[Serializable]
+public class Hit
+{
+	public string animation;
+	public string inputButton;
+	public float animationTime;
+	public float resetTime;
+
+	public int damage;
+	public AudioClip hitSound;
+	public bool slowDown;
+}
---
+++ PlayerCombo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombo : MonoBehaviour {
+
+	public Combo[] combos;
+
+	private Animator anim;
+
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CheckInputs();
+
+	}
+
+	void CheckInputs()
+	{
+		for (int i = 0; i < combos.Length; i++)
+		{
+			if (Input.GetButtonDown(combos[i].hits[0].inputButton))
+			{
+				PlayHit(combos[i].hits[0]);
+				break;
+			}
+		}
+	}
+
+	void PlayHit(Hit hit)
+	{
+		anim.Play(hit.animation);
+	}
+
+	void ResetCombo()
+	{
+
+	}
+}
